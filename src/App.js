070a7e098255a4c29c565cddd56b605c364ad452
louisfortin <louis.fortin@jstack.eu>
2020-06-05 18:34:22 +0200

    add assets and Router

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Redirect,
+  Route,
+} from "react-router-dom";
+// components
+import ReactApp from './components/ReactApp';
+import Home from './components/Home';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+        {/* A <Switch> looks through its children <Route>s and
+            renders the first one that matches the current URL. */}
+        <Switch>
+          <Route exact path="/">
+            <Redirect to="/home" />
+          </Route>
+          <Route exact path="/home">
+            <Home />
+          </Route>
+          <Route exact path="/thank-you">
+            <ReactApp />
+          </Route>
+          <Route>
+            <Home />
+          </Route>
+        </Switch>
+    </Router>
   );
 }
 
---
+++ src/components/Home.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import styled from 'styled-components/macro';
+
+const App = styled('div')`
+	text-align: center;
+`;
+
+const Home = () => (
+	<App>
+		List of whiskeys
+	</App>
+);
+
+export default Home;
